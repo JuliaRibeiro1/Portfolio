2478f6fe5dd29c557192a80daeb70582075828e3
unknown <hijulia259@gmail.com>
2023-04-24 11:09:07 -0300

    falta adcionar fotos dos projetos

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ import data from "./projectsData.js"
 
 console.log(data)
 const get = element => document.querySelector(element)
-
-get(".projects").innerHtml = getProjects()
-
+get(".projects").innerHTML = getProjects()
+console.log(get(".projects"))
 function getProjects() {
     let projectsPlaceholder = ""
     data.map(project => {
        projectsPlaceholder +=
-        `<div>
+        `<div class="project flex container">
+            <a href="${project.link}" target="_blank"></a>
             <h3>${project.name}</h3>
-            <a href="${project.link}"></a>
-        </div>`
+            
+        </div>
+        `
     })
+    console.log(projectsPlaceholder)
     return projectsPlaceholder
 }
+
+
