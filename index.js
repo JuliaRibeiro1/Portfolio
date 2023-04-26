d8a1d1145d4307745f62414e9be2d87580ef5591
unknown <hijulia259@gmail.com>
2023-04-26 14:16:12 -0300

    falta arrumar nav em telas maiores

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import data from "./projectsData.js"
 
 console.log(data)
 const get = element => document.querySelector(element)
-get(".projects").innerHTML = getProjects()
+get(".projects").innerHTML = getProjectsHtml()
 console.log(get(".projects"))
-function getProjects() {
+function getProjectsHtml() {
     let projectsPlaceholder = ""
     data.map(project => {
-        const {name,link,id} = project
+        const {name,link,image} = project
        projectsPlaceholder +=
-        `<div class="project flex container" id="project${id}">
-            <a href="${link}" target="_blank"></a>
+        `<div class="project flex container" ">
+            <a href="${link}" target="_blank" style=background-image:url(${image})></a>
             <h3>${name}</h3>
             
         </div>
--- projectsData.js
+++ projectsData.js
@@ -2,13 +2,15 @@
     {
         name:"Gerador de paleta de cores",
         link: "https://github.com/JuliaRibeiro1/Scrimba-projects/tree/main/Color%20Scheme%20Generator",
-        id:1
+        image:"images/color-scheme.png"
     },
     {
         name:"Jogo da forca",
         link:"https://github.com/JuliaRibeiro1/Hangman",
-        id:2
+        image: "images/hangman.png"
 
-    }
+    },
+        
+    
 ]
 export default data
