ca39bd35a95ba205dc338e75013bacdd5de0297f
unknown <hijulia259@gmail.com>
2023-08-25 18:47:33 -0300

    checkbox com neomorfismo

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function getProjectsHtml() {
 }
 
 get("#mode-checkbox").addEventListener("change",() => {
-    get(".intro-section").classList.toggle("section-dark-mode")
-    get("body").classList.toggle("body-dark-mode")
-    get(".contact-section").classList.toggle("section-dark-mode")
-    get(".footer-container").classList.toggle("footer-dark-mode")
-    get("header").classList.toggle("nav-dark-mode")
-    get(".linkedin").classList.toggle("linkedin-dark-mode")
-    get(".github").classList.toggle("github-dark-mode")
+    get(".intro-section").classList.toggle("section-light-mode")
+    get("body").classList.toggle("body-light-mode")
+    get(".contact-section").classList.toggle("section-light-mode")
+    get(".footer-container").classList.toggle("footer-light-mode")
+    get("header").classList.toggle("nav-light-mode")
+    get(".linkedin").classList.toggle("linkedin-light-mode")
+    get(".github").classList.toggle("github-light-mode")
 
 
   
